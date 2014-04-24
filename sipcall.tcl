20d8e9a9ff77556261672075817b6a779689b9ef
Vitaly Takmazov <vitalyster@gmail.com>
2014-04-24 16:27:59 +0400

    Statuses

--- sipcall.tcl
+++ sipcall.tcl
@@ -68,11 +68,9 @@ proc sipcall::load_stored_theme {args} {
 }
 
 proc sipcall::call_button {chatid type} {
-
     if {$type ne "chat"} {
 	return
     }
-    
     set cw [chat::winid $chatid]
 
     Button $cw.status.callbutton \
@@ -82,16 +80,75 @@ proc sipcall::call_button {chatid type} {
 	-width 24 \
 	-relief flat \
 	-state normal \
-	-command [namespace code [list show_info $chatid]]
+	-command [namespace code [list call_user $chatid]]
     pack $cw.status.callbutton -side right -after $cw.status.mb
 }
 
-proc sipcall::show_info {chatid} {
-    set jid [chat::get_jid $chatid]
-    if {[catch { set sipsrv [::xmpp::dns::resolveSIPU [::xmpp::jid::server $jid]] }]} {
-	tk_messageBox -message "$jid does not support calls" -type ok
+proc sipcall::call_user {chatid} {
+    variable sipctx
+    set jid [::xmpp::jid::removeResource [chat::get_jid $chatid]]
+    if {[info exists sipctx($jid)]} {
+	switch -- $sipctx($jid) {
+	    error {
+		return
+	    }
+	    progress {
+		[namespace current]::status $jid "Wait..."
+		return
+	    }
+	    call {
+		[namespace current]::status $jid "SIP call hangup"		
+		pjsip::hangup
+		set sipctx($jid) ok
+		return
+	    }
+	}
+    }
+    set sipctx($jid) progress
+    
+    if {[catch { ::xmpp::dns::resolveSIPU [::xmpp::jid::server $jid] }]} {
+	[namespace current]::status $jid "$jid does not support SIP calls"
+	set sipctx($jid) error
     } else {
-	pjsip::dial sip:[xmpp::jid::removeResource $jid]
+	[namespace current]::status $jid "Dialing..."
+	set sipctx($jid) call
+	pjsip::dial sip:$jid		    
+    }
+}
+
+proc sipcall::onregistered {id} {
+    puts "$id registered"
+}
+
+proc sipcall::onstatechanged {callno callid state args} {
+    puts $callno$callid$state$args
+}
+
+proc sipcall::status {jid text} {
+    set xlib [lindex [lindex $::connections 0] 0]
+    set chatid [::chat::chatid $xlib $jid]
+    ::chat::add_message $chatid $jid {info} $text {}
+}
+
+proc sipcall::onmedia {callNo accountID mediaState durationSec remoteInfo} {
+    tk_messageBox -message $mediaState$durationSec$remoteInfo -type ok
+}
+
+proc sipcall::onincoming {callNo accountID remoteInfo localInfo} {
+    variable sipctx
+    set remote [lindex [split $remoteInfo {:>}] 1]
+    set answer [tk_messageBox -message "Incoming call $remote, answer?" -type yesno]
+    switch -- $answer {
+	yes {
+	    [namespace current]::status $remote "SIP call started"	    
+	    pjsip::answer
+	    set sipctx($remote) call
+	}
+	no {
+	    [namespace current]::status $remote "SIP call rejected"
+	    pjsip::reject
+	    set sipctx($remote) ok
+	}
     }
 }
 
@@ -100,5 +157,9 @@ proc sipcall::register_sip {xlib} {
     set user [::xmpp::jid::node $jid]
     set server [::xmpp::jid::server $jid]
     set sipuri sip:[::xmpp::jid::removeResource $jid]
+    pjsip::notify <Registration> [namespace current]::onregistered
+    pjsip::notify <State> [namespace current]::onstatechanged
+    pjsip::notify <Incoming> [namespace current]::onincoming
+    pjsip::notify <Media> [namespace current]::onmedia
     pjsip::register sip:$server $sipuri $server $user $::loginconf(password) stun.jabber.ru
 }
